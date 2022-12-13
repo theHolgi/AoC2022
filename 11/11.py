6f3741808662e75614e0a2559275e3214dbb619a
Holger Lamm <Holger.Lamm@flexoptix.net>
2022-12-13 08:04:32 +0100

    Day 11 part 2

--- 11/11.py
+++ 11/11.py
@@ -10,13 +10,14 @@ class Monkey:
       self.worry_function = worry_function
       self.throwing_function = throwing_function
       self.activity = 0
+      self.factor = 1
 
    def throw(self) -> None:
       while len(self.stock) > 0:
          self.activity += 1
          item = self.stock.pop(0)
          logging.debug(f"{self.id} inspects {item} ")
-         item = self.worry_function(item) // 3
+         item = self.worry_function(item) % self.factor
          target = get_Monkey(self.throwing_function(item))
          logging.debug(f" ... becomes {item}, throw to {target.id}")
          target.get(item)
@@ -26,6 +27,9 @@ class Monkey:
 
    def __repr__(self) -> str:
       return f"🐒 {self.id}: 💥 {self.activity} {self.stock}"
+
+   def set_factor(self, factor):
+      self.factor = factor
 
 
 def get_Monkey(i) -> Monkey:
@@ -50,13 +54,18 @@ def create_puzzle_Monkeys() -> List[Monkey]:
            Monkey(6, [54, 55, 58], lambda i: i + 7, lambda i: 1 if i % 11 == 0 else 5),
            Monkey(7, [79, 51, 83, 88, 93, 76], lambda i: i * 3, lambda i: 0 if i % 2 == 0 else 6)
            ]
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 Monkeys.extend(create_puzzle_Monkeys())
+#factor = 23*19*13*17
+factor = 5*17*7*13*19*3*11*2
+for m in Monkeys:
+   m.set_factor(factor)
 
-for round in range(20):
+for round in range(10000):
    for monkey in Monkeys:
       monkey.throw()
-   logging.info(f"End of round {round+1}: {Monkeys}")
+   if (round+1) % 1000 == 0:
+      logging.info(f"End of round {round+1}: {Monkeys}")
 
 actives = sorted(Monkeys, key=lambda m: m.activity, reverse=True)
 
