bd5047fd64f976f0cbb928b3e010049d3ce4ff0c
Holger Lamm <Holger.Lamm@flexoptix.net>
2022-12-14 22:17:29 +0100

    Day 13 part 2

--- 13/13.py
+++ 13/13.py
@@ -1,46 +1,53 @@
 from json import JSONDecoder
 
+
+class Signal:
+   def __init__(self, value):
+      self.v = value
+   def __lt__(self, other):
+      return self.compare(other) == 1
+
+   def __gt__(self, other):
+      return self.compare(other) == -1
+
+   def __repr__(self) -> str:
+      return str(self.v) + "\n"
+
+   def compare(self,other) -> int:
+      if type(self.v)==int and type(other.v) == int:
+         if self.v < other.v:
+            return 1
+         elif self.v > other.v:
+            return -1
+         else:
+            return 0
+      elif type(self.v) == int:  # and r is list
+         return Signal([self.v]).compare(other)
+      elif type(other.v) == int:  # and l is list
+         return self.compare(Signal([other.v]))
+      else:  # both is list
+         if len(self.v) == 0 and len(other.v) == 0:
+            return 0
+         elif len(self.v) == 0:  # and r is not
+            return 1
+         elif len(other.v) == 0:  # and l is not
+            return -1
+         else:
+            res = Signal(self.v[0]).compare(Signal(other.v[0]))
+            if res!=0:
+               return res
+            else:
+               return Signal(self.v[1:]).compare(Signal(other.v[1:]))
+
 with open("13.txt") as f:
    lines = f.readlines()
+   lines = list(map(lambda v: Signal(JSONDecoder().decode(v)), filter(lambda s: s != "\n", lines)))
 
-def compare(l,r) -> int:
-   if type(l)==int and type(r) == int:
-      if l < r:
-         return 1
-      elif l > r:
-         return -1
-      else:
-         return 0
-   elif type(l) == int:  # and r is list
-      return compare([l], r)
-   elif type(r) == int:  # and l is list
-      return compare(l, [r])
-   else:  # both is list
-      if len(l) == 0 and len(r) == 0:
-         return 0
-      elif len(l) == 0:  # and r is not
-         return 1
-      elif len(r) == 0:  # and l is not
-         return -1
-      else:
-         res = compare(l[0], r[0])
-         if res!=0:
-            return res
-         else:
-            return compare(l[1:], r[1:])
+beacons = [Signal([[2]]), Signal([[6]])]
 
-index = 0
-sum = 0
-while len(lines)>0:
-   index+=1
-   l = JSONDecoder().decode(s=lines.pop(0))
-   r = JSONDecoder().decode(s=lines.pop(0))
-   if len(lines)>0:
-      lines.pop(0)
+sorted_lines = sorted(lines + beacons)
 
-   res = compare(l,r)
-   print(f"#{index}: {l} and {r} is {res}")
-   if res == 1:
-      sum += index
+p1 = sorted_lines.index(beacons[0])+1
+p2 = sorted_lines.index(beacons[1])+1
 
-print(f"Sum is {sum}")
+print(f"{sorted_lines}; {p1}, {p2} = {p1*p2}")
